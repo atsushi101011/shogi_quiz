a7d2b93186bef2a601f0f6fb9b54c3b36cdae5ab
atsushi101011 <atushi10112313@gmail.com>
2022-02-24 09:56:04 +0900

    トップページの作成

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <h2>観る将のための</h2>
+    <h1>将棋クイズ</h1>
+    <router-link to="#">クイズを解く</router-link>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+export default {
+  components: {
   }
+}
 </script>
