459850b8748267637b3162dbb8c738ebd4444e7c
atsushi101011 <atushi10112313@gmail.com>
2022-02-24 10:20:33 +0900

    questionsコントローラーの作成

---
+++ backend/app/controllers/v1/questions_controller.rb
@@ -0,0 +1,36 @@
+class V1::QuestionsController < ApplicationController
+  def index
+    questions = Question.all
+    render json: questions
+  end
+
+  def update
+    question = Question.find(params[:id])
+    if question.update(question_params)
+      render json: question
+    else
+      render json: question.errors
+    end
+  end
+
+  def create
+    question = Question.new(question_params)
+    if question.save
+      render json: question, status: :created
+    else
+      render json: question.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    question = Question.find(params[:id])
+    if question.destroy
+      render json: question
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+end
