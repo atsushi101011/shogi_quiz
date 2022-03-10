a63eb51dab12d2bf2980f1ec69cc352a2e91df7e
atsushi101011 <atushi10112313@gmail.com>
2022-03-10 10:23:46 +0900

    問題を一問ずつ表示するコンポーネントの作成

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <p>問題: {{this.$route.params.id}} {{ question.content }}</p>
-    <router-link to="/questions">Back</router-link>
+    <p>問題: {{this.$route.params.id}} {{ question().content }}</p>
+    <p>回答</p>
+        <p v-for="choice in question().choices" :key="choice.id">
+          <v-btn >{{ choice.content }}</v-btn>
+        </p>
+        <!-- <router-link to="/questions/">次の問題へ</router-link> -->
   </div>
 </template>
 
@@ -11,13 +15,11 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState(["questions"]),
+  },
+  methods: {
     question() {
-      return (
-        this.questions.find(
-          (question) => question.id === this.$route.params.id
-        )
-      );
-    },
+      return (this.questions[this.$route.params.id - 1])
+    }
   },
   created() {
     this.$store.dispatch("fetchQuestions");
