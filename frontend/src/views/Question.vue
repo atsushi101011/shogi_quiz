8f8108ff5373aab12cb38c9700579081865aab57
atsushi101011 <atushi10112313@gmail.com>
2022-03-10 15:22:33 +0900

    問題を順番に表示

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -5,8 +5,9 @@
         <p v-for="choice in question().choices" :key="choice.id">
           <v-btn @click="judgement(choice)">{{ choice.content }}</v-btn>
         </p>
-        <p>{{ correct_answer }}</p>
-        <!-- <router-link to="/questions/">次の問題へ</router-link> -->
+        {{ correct_answer }}
+    <!-- 最後の問題のときは結果ページへ遷移させる -->
+    <router-link :to="{ name: 'show-question', params: { id: nextQuestion(this.$route.params.id) }}">次の問題へ</router-link>
   </div>
 </template>
 
@@ -25,13 +26,21 @@ export default {
 
   methods: {
     question() {
-      return (this.questions[this.$route.params.id - 1])
+      return (this.questions[this.$route.params.id - 1]) //idを取得しているというより、配列の並びに対応した問題を取得している
     },
     judgement(choice) {
       if (choice.is_answer) {
         this.correct_answer = "正解!!";
       } else {
         this.correct_answer = "不正解!!";
+      }
+    },
+    nextQuestion(number) {  //次の問題へ進むのに必要な更新をこの関数で定義する
+      this.quizCount = Number(number) + 1;
+      if (this.quizCount <= this.questions.length) {
+        return this.quizCount;
+      } else {
+        return 1;
       }
     },
   },
