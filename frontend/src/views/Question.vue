19f0f07c32f01f23af6f9c21ee7597842a44389e
atsushi101011 <atushi10112313@gmail.com>
2022-03-10 10:52:10 +0900

    正誤判定の描画を実装

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -3,8 +3,9 @@
     <p>問題: {{this.$route.params.id}} {{ question().content }}</p>
     <p>回答</p>
         <p v-for="choice in question().choices" :key="choice.id">
-          <v-btn >{{ choice.content }}</v-btn>
+          <v-btn @click="judgement(choice)">{{ choice.content }}</v-btn>
         </p>
+        <p>{{ correct_answer }}</p>
         <!-- <router-link to="/questions/">次の問題へ</router-link> -->
   </div>
 </template>
@@ -16,11 +17,25 @@ export default {
   computed: {
     ...mapState(["questions"]),
   },
+  data() {
+    return {
+      correct_answer: null,
+    }
+  },
+
   methods: {
     question() {
       return (this.questions[this.$route.params.id - 1])
-    }
+    },
+    judgement(choice) {
+      if (choice.is_answer) {
+        this.correct_answer = "正解!!";
+      } else {
+        this.correct_answer = "不正解!!";
+      }
+    },
   },
+
   created() {
     this.$store.dispatch("fetchQuestions");
   },
