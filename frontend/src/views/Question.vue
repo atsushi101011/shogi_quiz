b455b8dbe8c30676b2cec8a01250605f3303ec95
atsushi101011 <atushi10112313@gmail.com>
2022-03-15 21:02:41 +0900

    questionにcorectAnswerフラグの追加

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -3,10 +3,9 @@
     <p>問題: {{this.$route.params.id}} {{ question().content }}</p>
     <p>回答</p>
         <p v-for="choice in question().choices" :key="choice.id">
-          <v-btn @click="judgement(choice)">{{ choice.content }}</v-btn>
+          <v-btn @click="judgement(choice, question())">{{ choice.content }}</v-btn>
         </p>
-        {{ correctAnswer }}
-    <!-- 最後の問題のときは結果ページへ遷移させる quizCount === numberOfQuestionsのとき-->
+        {{ question().correctAnswer }}
     <p v-if="this.quizCount < this.numberOfQuestions">
       <router-link :to="nextQuestion()">次の問題へ</router-link>
     </p>
@@ -25,7 +24,6 @@ export default {
   },
   data() {
     return {
-      correctAnswer: null, //各question内にあれば、問題ごとの正誤がわかる
       numberOfQuestions: 10,
       quizCount: 0,
       correctCount: 0,
@@ -35,14 +33,15 @@ export default {
   methods: {
     question() {
       let question = this.questions[this.$route.params.id - 1]; //question.idを取得しているというより、配列の並びに対応した問題を取得している,ここを要素数が上限のランダムな数字にすればいい
-      // question.push(this.correctAnswer);
-      return (question)
+      question["correctAnswer"] = false;
+      // console.log(typeof(question.correctAnswer))
+      return (question);
     },
-    judgement(choice) {
+    judgement(choice, question) {
       if (choice.is_answer) {
-        this.correctAnswer = "正解!!"; //true、falseでそのときに正解を表示する方がいい
+        question.correctAnswer = true;
       } else {
-        this.correctAnswer = "不正解!!";
+        question.correctAnswer = false;
       }
     },
     nextQuestion() {  //次の問題へ進むのに必要な更新をこの関数で定義する
