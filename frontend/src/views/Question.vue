8cab7f4c7be657df757efc8b231f6e633de78438
atsushi101011 <atushi10112313@gmail.com>
2022-03-16 11:40:45 +0900

    question関数をcomputedに記述

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-    <p>問題: {{this.$route.params.id}} {{ question().content }}</p>
+    <p>問題: {{this.$route.params.id}} {{ question.content }}</p>
     <p>回答</p>
-        <p v-for="choice in question().choices" :key="choice.id">
-          <v-btn @click="judgement(choice, question())">{{ choice.content }}</v-btn>
+        <p v-for="choice in question.choices" :key="choice.id">
+          <v-btn @click="judgement(choice, question)">{{ choice.content }}</v-btn>
         </p>
-        {{ question().correctAnswer }}
     <p v-if="this.quizCount < this.numberOfQuestions">
       <router-link :to="nextQuestion()">次の問題へ</router-link>
     </p>
     <p v-else-if="this.quizCount === this.numberOfQuestions">
       <router-link :to="{name:'result'}">結果ページへ</router-link>
     </p>
+
+    <br><br>{{ question }} <!-- 確認用 -->
   </div>
 </template>
 
@@ -21,6 +22,12 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState(["questions"]),
+    question() {
+      let question = this.questions[this.$route.params.id - 1];
+      // question["correctAnswer"] = false;
+      console.log(question);
+      return(question);
+    },
   },
   data() {
     return {
@@ -31,17 +38,13 @@ export default {
   },
 
   methods: {
-    question() {
-      let question = this.questions[this.$route.params.id - 1]; //question.idを取得しているというより、配列の並びに対応した問題を取得している,ここを要素数が上限のランダムな数字にすればいい
-      question["correctAnswer"] = false;
-      // console.log(typeof(question.correctAnswer))
-      return (question);
-    },
     judgement(choice, question) {
       if (choice.is_answer) {
         question.correctAnswer = true;
+        console.log(question.correctAnswer);
       } else {
         question.correctAnswer = false;
+        console.log(question.correctAnswer);
       }
     },
     nextQuestion() {  //次の問題へ進むのに必要な更新をこの関数で定義する
