7b37bfd1e49a457e1d448f068fa9550691df08ea
atsushi101011 <atushi10112313@gmail.com>
2022-03-22 12:33:02 +0900

    #23 正解数をカウント

--- frontend/src/views/Question.vue
+++ frontend/src/views/Question.vue
@@ -73,7 +73,7 @@ export default {
       this.isActive = true;
       if (choice.is_answer) {
         question.correctAnswer = true;
-        // 正解数をカウント
+        this.correctCount++;
         return(question.correctAnswer);
       } else {
         question.correctAnswer = false;
