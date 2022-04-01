60ea3601b2579bb5d4c37473f01e6f956e9f4247
atsushi101011 <atushi10112313@gmail.com>
2022-04-01 09:51:00 +0900

    seedファイルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+  enum role: { general: 0, admin: 1 }
 end
