be4c95c70c5f71604f3eaf575559a174654dba3c
atsushi101011 <atushi10112313@gmail.com>
2022-04-01 10:07:35 +0900

    ユーザー登録、ログイン機能の追加

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,16 @@
+class UserSessionsController < ApplicationController
+  skip_before_action :require_login
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_back_or_to rails_admin_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :questions, only: [:create, :destroy, :index, :show, :update]
   end
+
+  resources :users, only: [:new, :create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
